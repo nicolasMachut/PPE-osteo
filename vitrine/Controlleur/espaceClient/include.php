213d9d5cafae8042e3204c5a8d82eb16dc410d54
nicolasMachut <machut.nicolas@live.fr>
2013-05-28 15:46:33 +0200

    mep2

---
+++ vitrine/Controlleur/espaceClient/include.php
@@ -0,0 +1,18 @@
+<?php
+	
+	$compte = array(
+		array(
+		'libelle' => 'info',
+		'texte' => 'Vos informations personnelles'
+		),
+		array(
+		'libelle' => 'rdv',
+		'texte' => 'Vos rendez-vous'
+		),
+		array(
+		'libelle' => 'newRdv',
+		'texte' => 'Réserver du temps dans une salle'
+		)
+	);
+
+
